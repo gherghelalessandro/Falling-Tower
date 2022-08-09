1d05b82c5e872f800c0629f2d110ac590ccb6711
Gherghel Alessandro <101932535+gherghelalessandro@users.noreply.github.com>
2022-08-09 10:21:39 +0300

    menu-functionality

---
+++ Assets/Scripts/LoadFunction.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadFunction : MonoBehaviour
+{
+    public void loadScene(int sceneindex)
+    {
+        SceneManager.LoadScene(sceneindex);
+    }
+    public void exitthegame()
+    {
+        Application.Quit();
+    }
+}
